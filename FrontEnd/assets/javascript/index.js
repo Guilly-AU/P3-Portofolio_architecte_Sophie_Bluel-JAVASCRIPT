ba9304f325144bf1e6318ad1eb5a155f687a236c
guilly <guillaume.damenez@gmail.com>
2023-03-22 16:24:24 +0800

    function-filters

--- FrontEnd/assets/javascript/index.js
+++ FrontEnd/assets/javascript/index.js
@@ -5,6 +5,10 @@ function getAllWorks() {
             if (res.ok) {
                 res.json().then(result => {
                     createArticle(result);
+                    filtersAll(result);
+                    filterObjets(result);
+                    filterApartments(result);
+                    filterHotels(result);
                     console.log(result);
                 });
             }
@@ -41,9 +45,48 @@ function createButton(text, className) {
     filters.appendChild(button);
 }
 // Call of the function to create the filter buttons
-createButton('Tous', 'btn-tous');
-createButton('Objets', 'btn-objets');
-createButton('Appartements', 'btn-appartements');
+createButton('Tous', 'btn-all');
+createButton('Objets', 'btn-objects');
+createButton('Appartements', 'btn-apartments');
 createButton(`Hôtels & restaurants`, 'btn-hotels');
+
+function filtersAll(result) {
+
+    const buttonAll = document.querySelector('#btn-all');
+    buttonAll.addEventListener('click', function () {
+        const allObjects = result.filter(obj => obj.categoryId != null);
+        document.querySelector('.gallery').innerHTML = '';
+        createArticle(allObjects);
+    })
+}
+// Creation des filtres
+function filterObjets(result) {
+
+    const buttonObjects = document.querySelector('#btn-objects');
+    buttonObjects.addEventListener('click', function () {
+        const filteredObjects = result.filter(obj => obj.categoryId === 1);
+        document.querySelector('.gallery').innerHTML = '';
+        createArticle(filteredObjects);
+    });
+
+};
+function filterApartments(result) {
+
+    const buttonApartments = document.querySelector('#btn-apartments');
+    buttonApartments.addEventListener('click', function () {
+        const filteredApartments = result.filter(obj => obj.categoryId === 2);
+        document.querySelector('.gallery').innerHTML = '';
+        createArticle(filteredApartments);
+    });
+};
+function filterHotels(result) {
+    
+    const buttonHotels = document.querySelector('#btn-hotels');
+    buttonHotels.addEventListener('click', function () {
+        const filteredHotels = result.filter(obj => obj.categoryId === 3);
+        document.querySelector('.gallery').innerHTML = '';
+        createArticle(filteredHotels);
+    });
+};
 
 getAllWorks();
