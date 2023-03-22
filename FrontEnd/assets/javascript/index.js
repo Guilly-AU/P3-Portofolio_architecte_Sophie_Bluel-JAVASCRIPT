64712a8b1a6daf7796639d41087ddd78a2107136
guilly <guillaume.damenez@gmail.com>
2023-03-22 15:52:15 +0800

    display-article-dynamic

--- FrontEnd/assets/javascript/index.js
+++ FrontEnd/assets/javascript/index.js
@@ -4,11 +4,32 @@ function getAllWorks() {
         .then(res => {
             if (res.ok) {
                 res.json().then(result => {
+                    createArticle(result);
                     console.log(result);
                 });
             }
             
         }); 
 }
+// Creation of article
+function createArticle(result) {
+    let sectionArticle = document.querySelector(".gallery");
+    // For loop
+    for (let i = 0; i < result.length; i++) {
+        let article = result[i];
+        // Retrieval of the DOM element that will host the article
+        let articleElement = document.createElement("figure");
+        articleElement.dataset.id = article.id;
+        let imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        let nomElement = document.createElement("p");
+        nomElement.innerText = article.title;
+       
+        articleElement.append(imageElement);
+        articleElement.append(nomElement);
+        sectionArticle.append(articleElement);
+    }
+}
+
 
 getAllWorks();
