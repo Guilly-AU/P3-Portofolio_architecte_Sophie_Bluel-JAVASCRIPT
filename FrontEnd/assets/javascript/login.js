7b0eb6849103cfe802a70a156b0896cd18c9be2c
guilly <guillaume.damenez@gmail.com>
2023-03-29 13:56:43 +0800

    js-login

--- FrontEnd/assets/javascript/login.js
+++ FrontEnd/assets/javascript/login.js
@@ -1,24 +1,19 @@
-// Attach click event listenner to login button
 document.querySelector('#btn-login').onclick = (e) => {
   e.preventDefault();
-  // Get the email and password values entered by the user
   let emailAddress = document.getElementById('email').value;
   let password = document.getElementById('password').value;
   let error;
-  // Create a user object with email and password properties
   let user = {
     email: emailAddress,
     password: password
     };
-  // If the password is not entered, set an error message
+    
   if (!password) {
     error = "Veuillez renseigner un mot de passe";
   }
-  // If the email is not entered, set an error message
   if (!emailAddress) {
     error = "Veuillez renseigner un email";
   }
-  // If there is an error, display it and return false to prevent form submission
   if (error) {
     e.preventDefault();
     document.getElementById('error').innerHTML = error;
@@ -27,10 +22,9 @@ document.querySelector('#btn-login').onclick = (e) => {
     console.log(emailAddress);
     console.log(password);
     console.log(user);
-    // Convert user object to JSON string
     const json = JSON.stringify(user);
   }
-  // Send a POST request to login endpoint with user object as request body 
+   
   fetch("http://localhost:5678/api/users/login", {
     method: 'POST',
     headers: {
@@ -44,15 +38,15 @@ document.querySelector('#btn-login').onclick = (e) => {
       console.log(loggedInUser);
   });
 }
-// Function to validate the loggedInUser object returned from the server
+
 function validateUser(loggedInUser) {
   const userId = loggedInUser?.userId;
-  // If the userId exists, save the token to localStorage and display a welcome message
   if (userId !=null) {
     localStorage.setItem('token', loggedInUser.token);
-    alert('Bienvenue');
+    document.location.href='./index.html'
   } else {
     document.getElementById('error').innerHTML = "Erreur dans l'identifiant ou le mot de passe";
   }
   console.log(userId);
 }
+console.log(token);
